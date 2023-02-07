8f41789c46ad90a2db61715e22af0be26a2647c0
iyzigaming <121947178+iyzigaming@users.noreply.github.com>
2023-02-07 21:23:42 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ app = Flask(__name__)
 CORS(app)
 @app.route('/demo',methods=['GET'])
 def demo():
-    r = requests.head("http://germanydnsx11.com:8000/ZbLQXUxk/Es272827../89061", allow_redirects=True)
-    return r.url
+    r = requests.head("http://germanydnsx11.com:8000/ZbLQXUxk/Es272827../89036", allow_redirects=False)
+    r.raise_for_status()
+    if 300 < r.status_code < 400:
+        url = r.headers.get('Location', url)
 
+    return url
 
 if __name__ == '__main__':
     app.run()
