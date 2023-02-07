d4988ce0eb75af81854fcafc3739af440f71d766
iyzigaming <121947178+iyzigaming@users.noreply.github.com>
2023-02-07 21:18:00 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -5,25 +5,10 @@ from flask_cors import CORS
 import re
 app = Flask(__name__)
 CORS(app)
-@app.route('/demo')
+@app.route('/demo',methods=['GET'])
 def demo():
-    headers = {
-        "accept": "*/*",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "tr-TR, tr;q = 0.9",
-        "origin": "https://www.maltinok.com",
-        "referer": "https://www.maltinok.com/",
-        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'cross-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-    }
-    ts = requests.get("http://germanydnsx11.com:8000/ZbLQXUxk/Es272827../89036",allow_redirects = True)
-    tsal = ts.text
-    return tsal
+    ts = requests.get("http://germanydnsx11.com:8000/ZbLQXUxk/Es272827../89061",allow_redirects = True)
+    return ts.text
 
 
 if __name__ == '__main__':
