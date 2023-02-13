4e840c654f0c3ddad507486d5b8892768d5d578c
iyzigaming <121947178+iyzigaming@users.noreply.github.com>
2023-02-13 22:33:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ def getm3u8():
     }
     ts = requests.get(source, headers=headers)
     tsal = ts.text
-    return tsal
     tsal = tsal.split('"')
     host = tsal[2].replace("\n","")
     return "https://testyayin.herokuapp.com/getcss?source="+host
