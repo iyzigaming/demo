83270ae72c824fbc173c854a8442bc35f0771851
iyzigaming <121947178+iyzigaming@users.noreply.github.com>
2023-02-16 22:41:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -31,12 +31,31 @@ def getm3u8():
         source2 = source.replace("https://","").split("/")
         source2 = source2[2].replace(".m3u8","")
         source = source.replace("https://","").split("/")
-        tsal = tsal.replace(source2+"/1","https://testyayin.herokuapp.com/getcss?source=https://"+source[0]+"/"+source[1]+"/"+source2+"/1")
+        tsal = tsal.replace(source2+"/1","https://testyayin.herokuapp.com/getjson?source=https://"+source[0]+"/"+source[1]+"/"+source2+"/1")
         return tsal
-    #tsal = tsal.split('"')
-    #host = tsal[2].replace("\n","")
     return tsal
     
+    
+@app.route('/getjson',methods=['GET'])
+def getjson():
+    source = request.args.get("source")
+    headers = {
+'accept': '*/*',
+'accept-language': 'tr-TR,tr;q=0.9',
+'origin': 'https://kalebettv29.com',
+'referer': 'https://kalebettv29.com/',
+'sec-ch-ua': '"Opera";v="95", "Chromium";v="109", "Not;A=Brand";v="24"',
+'sec-ch-ua-mobile': '?0',
+'sec-ch-ua-platform': '"Windows"',
+'sec-fetch-dest': 'empty',
+'sec-fetch-mode': 'cors',
+'sec-fetch-site': 'cross-site',
+'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 OPR/95.0.0.0'
+    }
+    ts = requests.get(source, headers=headers)
+    ts = ts.text
+    return ts  
+
     
 @app.route('/getcss',methods=['GET'])
 def getcss():
